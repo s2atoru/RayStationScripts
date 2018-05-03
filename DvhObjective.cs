3eb0f3af366f104791bf3d9a782cc38a2d9dda49
Satoru Sugimoto <ssugimot@juntendo.ac.jp>
2018-05-03 18:20:44 +0900

    Add DvhObjective

---
+++ DvhObjective.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juntendo.MedPhys
+{
+    public enum DvhType { Dose, Volume };
+    public enum DvhTargetType { Max, Min, Mean, Upper, Lower, Spare };
+    public enum DvhPresentationType { Abs, Rel };
+
+    public class DvhObjective
+    {
+        public string StructureName { get; set; }
+        public string StructureNameTps { get; set; }
+
+        public DvhType  Type { get; private set; }
+        public DvhTargetType TargetType { get; private set; }
+        public DvhPresentationType ValueType { get; private set; }
+
+        public double Volume { get; private set; }
+        public DvhPresentationType ArgumentType { get; set; }
+
+        public double Tolerance { get; private set; }
+        public double AcceptableLimit { get; private set; }
+
+        public string Remarks { get; set; }
+    }
+}
