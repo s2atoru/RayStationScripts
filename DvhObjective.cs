59c0fcf2794e28dd42575f85dad7e7f450e982ad
Satoru SUGIMOTO <s2atoru@gmail.com>
2018-05-04 14:40:52 +0900

    Change DvhObjective to BindableBase

--- DvhObjective.cs
+++ DvhObjective.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using System.IO;
+using Prism.Mvvm;
 
 namespace Juntendo.MedPhys
 {
@@ -7,21 +8,115 @@ namespace Juntendo.MedPhys
     public enum DvhTargetType { Max, Min, Mean, Upper, Lower, Spare };
     public enum DvhPresentationType { Abs, Rel };
 
-    public class DvhObjective
+    public class DvhObjective : BindableBase
     {
-        public string StructureName { get; set; }
-        public string StructureNameTps { get; set; }
+        private string title;
+        public string Title {
+            get { return title; }
+            set
+            {
+                this.SetProperty(ref this.title, value);
+            }
+        }
 
-        public DvhType Type { get; private set; }
-        public DvhTargetType TargetType { get; private set; }
-        public DvhPresentationType ValueType { get; private set; }
+        private string structureName;
+        public string StructureName {
+            get { return structureName; }
+            set
+            {
+                this.SetProperty(ref this.structureName, value);
+            }
+        }
 
-        public double Volume { get; private set; }
-        public DvhPresentationType ArgumentType { get; set; }
+        private string structureNameTps;
+        public string StructureNameTps
+        {
+            get { return structureNameTps; }
+            set
+            {
+                this.SetProperty(ref this.structureNameTps, value);
+            }
+        }
 
-        public double Tolerance { get; private set; }
-        public double AcceptableLimit { get; private set; }
+        private DvhType type; 
+        public DvhType Type {
+            get { return type; }
+            set
+            {
+                this.SetProperty(ref this.type, value);
+            }
+        }
 
-        public string Remarks { get; set; }
+        private DvhTargetType targetType;
+        public DvhTargetType TargetType {
+            get { return targetType; }
+            private set
+            {
+                this.SetProperty(ref this.targetType, value);
+            }
+        }
+
+        private DvhPresentationType valueType;
+        public DvhPresentationType ValueType {
+            get { return valueType; }
+            private set
+            {
+                this.SetProperty(ref this.valueType, value);
+            }
+        }
+
+        private double volume;
+        public double Volume {
+            get { return volume; }
+            private set
+            {
+                this.SetProperty(ref this.volume, value);
+            }
+        }
+
+        private DvhPresentationType argumentType;
+        public DvhPresentationType ArgumentType {
+            get { return argumentType; }
+            private set
+            {
+                this.SetProperty(ref this.argumentType, value);
+            }
+        }
+
+        private double tolerance;
+        public double Tolerance {
+            get { return tolerance; }
+            private set
+            {
+                this.SetProperty(ref this.tolerance, value);
+            }
+        }
+
+        private double acceptableLimit;
+        public double AcceptableLimit {
+            get { return acceptableLimit; }
+            private set
+            {
+                this.SetProperty(ref this.acceptableLimit, value);
+            }
+        }
+
+        private string remarks;
+        public string Remarks {
+            get { return remarks; }
+            set
+            {
+                this.SetProperty(ref this.remarks, value);
+            }
+        }
+
+        private bool isPassed;
+        public bool IsPassed {
+            get { return isPassed; }
+            set
+            {
+                this.SetProperty(ref this.isPassed, value);
+            }
+        }
     }
 }
