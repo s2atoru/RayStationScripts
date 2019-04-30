8225bacbf7594b4a74ee530cd3ba4136dd85cf0e
Satoru SUGIMOTO <s2atoru@gmail.com>
2019-04-30 21:01:16 +0900

    Add DoseVolumeIndex

---
+++ DoseVolumeIndex/Models/DoseVolumeIndex.cs
@@ -0,0 +1,21 @@
+﻿namespace DoseVolumeIndex.Models
+{
+    public class DoseVolumeIndex
+    {
+        public enum ValueType { Dose, Volume }
+        public enum Type { Dvh, Average }
+        public enum Unit { Relative, Absolute }
+
+        public string Title { get; set; }
+        public ValueType TargetValueType { get; set; }
+        public Type TargetType { get; set; }
+        public Unit TargetUnit { get; set; }
+        public Unit ArgumentUnit { get; set; }
+
+        public double ArgumentValue { get; set; }
+        public double Value {get; private set;}
+
+        public string RoiName { get; set; }
+        public string RoiNameTps { get; set; }
+    }
+}
