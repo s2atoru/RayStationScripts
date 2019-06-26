8d6a9742113103cb877a57dcee552f2b892d3b4a
Satoru SUGIMOTO <s2atoru@gmail.com>
2019-06-26 14:48:32 +0900

    Squashed 'MvvmCommon/' content from commit 47afe14
    git-subtree-dir: MvvmCommon
    git-subtree-split: 47afe143ebe90d583754d3663392595d05985bbe

---
+++ MvvmCommon/ViewModels/ListBoxItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+
+namespace MvvmCommon.ViewModels
+{
+    public class ListBoxItemViewModel : BindableBase
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { SetProperty(ref isSelected, value); }
+        }
+    }
+}
