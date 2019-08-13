952f31a77c6aabfdfb3e70ae85f279cbad87a900
Satoru SUGIMOTO <s2atoru@gmail.com>
2019-08-13 16:47:38 +0900

    Add BrainDoseIndices

---
+++ BrainDoseIndices/ViewModels/DoseIndicesViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace BrainDoseIndices.ViewModels
+{
+    public class DoseIndicesViewModel : IDisposable
+    {
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
+
+        public List<string> StructureNames { get; set; }
+
+        [Required(ErrorMessage = "Choose Target")]
+        public ReactiveProperty<string> TargetRx { get; private set; }
+        public ReactiveProperty<string> TargetRxErrorMessage { get; }
+
+        [Required(ErrorMessage = "Choose Dose volume")]
+        public ReactiveProperty<string> DoseVolumeRx { get; private set; }
+        public ReactiveProperty<string> DoseVolumeRxErrorMessage { get; }
+
+        public ReactiveCommand OkCommand { get; }
+
+        public ReactiveCommand CancelCommand { get; }
+
+        public bool IsOk { get; set; }
+
+        public DoseIndicesViewModel()
+        {
+            StructureNames = new List<string>{ "PTV", "zDose100", "zDosePrescription" };
+
+            TargetRx = new ReactiveProperty<string>().SetValidateAttribute(() => TargetRx).AddTo(this.Disposable);
+            TargetRxErrorMessage = TargetRx.ObserveErrorChanged
+            .Select(x => x?.OfType<string>()?.FirstOrDefault())
+            .ToReactiveProperty().AddTo(this.Disposable);
+
+            DoseVolumeRx = new ReactiveProperty<string>().SetValidateAttribute(() => DoseVolumeRx).AddTo(this.Disposable);
+            DoseVolumeRxErrorMessage = DoseVolumeRx.ObserveErrorChanged
+            .Select(x => x?.OfType<string>()?.FirstOrDefault())
+            .ToReactiveProperty().AddTo(this.Disposable);
+
+            OkCommand = new[] { TargetRx.ObserveHasErrors, DoseVolumeRx.ObserveHasErrors }.CombineLatest(x => x.All(y => !y)).ToReactiveCommand().AddTo(Disposable);
+            OkCommand.Subscribe(() => IsOk = true).AddTo(Disposable);
+
+            CancelCommand = new ReactiveCommand();
+            CancelCommand.Subscribe(() => IsOk = false).AddTo(Disposable);
+        }
+
+        public void Dispose()
+        {
+            this.Disposable.Dispose();
+        }
+    }
+}
---
+++ BrainDoseIndices/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace BrainDoseIndices.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new ViewModels.DoseIndicesViewModel();
+        }
+    }
+}
