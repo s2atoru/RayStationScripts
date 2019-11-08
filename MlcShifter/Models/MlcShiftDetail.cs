201c845b24a3aea82957e5a6779cf2ceea9244ad
Satoru SUGIMOTO <s2atoru@gmail.com>
2019-11-08 21:31:28 +0900

    Implement DataGrid

---
+++ MlcShifter/Models/MlcShiftDetail.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MlcShifter.Models
+{
+    public class MlcShiftDetail : BindableBase
+    {
+        private int leafNumber;
+        public int LeafNumber
+        {
+            get { return leafNumber; }
+            set { SetProperty(ref leafNumber, value); }
+        }
+
+        private bool isCheckedA = false;
+        public bool IsCheckedA
+        {
+            get { return isCheckedA; }
+            set { SetProperty(ref isCheckedA, value); }
+        }
+
+        private bool isCheckedB = false;
+        public bool IsCheckedB
+        {
+            get { return isCheckedB; }
+            set { SetProperty(ref isCheckedB, value); }
+        }
+
+        private double shiftA;
+        public double ShiftA
+        {
+            get { return shiftA; }
+            set { SetProperty(ref shiftA, value); }
+        }
+
+        private double shiftB;
+        public double ShiftB
+        {
+            get { return shiftB; }
+            set { SetProperty(ref shiftB, value); }
+        }
+    }
+}
---
+++ MlcShifter/ViewModels/MlcShifterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MlcShifter.ViewModels
+{
+    public class MlcShifterViewModel : BindableBase
+    {
+        public ObservableCollection<Models.MlcShiftDetail> MlcDetails { get; set; } = new ObservableCollection<Models.MlcShiftDetail>();
+
+        public int NumberOfLeafPairs = 60;
+
+        public MlcShifterViewModel()
+        {
+            for (int i =0; i<NumberOfLeafPairs; i++)
+            {
+                MlcDetails.Add(new Models.MlcShiftDetail { LeafNumber = NumberOfLeafPairs -i , IsCheckedA = false, IsCheckedB = false, ShiftA = 0, ShiftB = 0 });
+            }
+        }
+    }
+}
